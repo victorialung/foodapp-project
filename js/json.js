bdce38e8f9d502e64673f33c960e5bd6ccca1833
Victoria <vtr.lung@outlook.es>
2018-01-02 11:53:03 -0500

    subiendo html recuperado-primera vista

--- js/json.js
+++ js/json.js
@@ -1,30 +1,29 @@
 var json = {
-  'Miraflores': [
-    {'Vegan Food': [
-      {'dirección': 'Calle Las Carnecitas 480'},
-      {'platos': 'Piqueo vegano',
-        'precio': '$20.00',
-        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
-      },
-      {'platos': 'Ensalada primaveral',
-        'precio': '$25.00',
-        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
-      },
-    ]
+  'veganFood': [
+    {'dirección': 'Calle Las Carnecitas 480'},
+    {'platos': 'Piqueo vegano',
+      'precio': '$20.00',
+      'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
     },
-    {'Eat Your Steak': [
-      {'dirección': 'Calle Las Verduritas 480'},
-      {'platos': 'Piqueo carnivoro',
-        'precio': '$20.00',
-        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
-      },
-      {'platos': 'Parrilla primaveral',
-        'precio': '$25.00',
-        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
-      },
-    ]
-    }
+    {'platos': 'Ensalada primaveral',
+      'precio': '$25.00',
+      'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
+    },
   ]
+  ,
+  'eatYourSteak': [
+    {'dirección': 'Calle Las Verduritas 480'},
+    {'platos': 'Piqueo carnivoro',
+      'precio': '$20.00',
+      'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
+    },
+    {'platos': 'Parrilla primaveral',
+      'precio': '$25.00',
+      'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff'
+    },
+  ]
+
+
 };
 
 // $.each(json, function(i, item) {
